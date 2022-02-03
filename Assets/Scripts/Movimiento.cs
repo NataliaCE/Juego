1ddd2c4a20fac49dbee58c7d1fc5904b883e7794
Natalia Canudas <natalia.200351@hotmail.com>
2022-02-03 14:14:04 +0100

    FEB 3

--- Assets/Scripts/Movimiento.cs
+++ Assets/Scripts/Movimiento.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class Movimiento : MonoBehaviour
 {
+    Rigidbody2D rb;
+    public float speed;
+    [SerializeField] Animator animacion;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Prueba
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            Move();
+        }
+    }
+
+    void Move()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float moveBy = speed * x;
+        animacion.SetFloat("Direccion", x);
+        rb.velocity = new Vector2(moveBy, rb.velocity.y);
     }
 }
